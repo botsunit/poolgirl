b9b49a4f8f3cc18295936fc17e4a5ec872e03771
glejeune <gregoire.lejeune@free.fr>
2016-06-16 16:51:35 +0200

    Prepare 0.0.2

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Poolgirl.Mixfile do
 
   def application do
     [
-       applications: [:kernel, :stdlib],
+       applications: [],
        env: [],
        mod: {:poolgirl_app, []}
     ]

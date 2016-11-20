b2b7d0b1dc809a9518bbc467123d40405287713d
glejeune <gregoire.lejeune@free.fr>
2016-11-20 18:10:58 +0100

    Bump to 1.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Poolgirl.Mixfile do
   def project do
     [
       app: :poolgirl,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
